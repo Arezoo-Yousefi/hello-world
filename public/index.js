f406c86c4b8bb0059bc9df7172289d4fe0fea2e7
are_yousefi@yahoo.com <are_yousefi@yahoo.com>
2022-11-04 14:14:00 -0700

    test

--- public/index.js
+++ public/index.js
@@ -1,3 +1,15 @@
+let userName;
+let myName;
+let myMessage;
+const submitName = function() {
+    const user = document.getElementById("userName").value;
+    userName = user;
+    const attribute = document.getElementById("hidden1");
+    attribute.hidden = true;
+    const changeAttribute = document.getElementById("hidden2");
+    changeAttribute.hidden = false;
+    
+}
 const Reload = function() {
 const lists = fetch('/getMessages')
 .then(function(response) {
@@ -5,9 +17,13 @@ const lists = fetch('/getMessages')
 })
 .then(
     function(data) { 
-        const pArray = data.messages;
+        const pArray = data.messages.map((value)=>{
+                return`<span id="myName" style="color: rgb(69, 134, 69);">${value.name}: </span><span id="myMessage" style="font-weight: bold ;">${value.message}</span>`
+            })
+        
         const p = document.getElementById("messages");;
         p.innerHTML = pArray.join('<BR/>');
+        
     });
 }
 
@@ -19,8 +35,9 @@ const sendMessage = function () {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"message": message})
+    body: JSON.stringify({"name": userName,"message": message})
     });
+    document.getElementById("message").value = "";
 }
 
 setInterval(Reload, 100);
--- server.js
+++ server.js
@@ -1,23 +1,53 @@
 const express = require ("express");
 const app = express();
+const sql = require('mssql');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+const sqlConfig = {
+    user: 'arezoo',
+    password: 'M@nti0431',
+    database: 'ArezooDB',
+    server: 'localhost',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+    }
+}
 
 const port = 3100;
-messageArray = [];
+const getData = async function () {
+    await sql.connect(sqlConfig);
+    const result = await sql.query`select * from chat`;
+    return result.recordset;
+}
+const setData = function (nam, mes) {
+    sql.connect(sqlConfig);
+    sql.query`insert into chat ([name], [message]) values (${nam}, ${mes})`;
+}
 
 app.use("/", express.static("public"));
 
 app.get("/getMessages", (req, res) => {
-    res.json({messages: messageArray})
+    getData().then(function(data){
+        res.json({messages: data})
+    });
 })
 
 app.post("/postMessage", (req, res) =>{
-    const msg = req.body.message;
-    messageArray.push(msg);
+    nme = req.body.name;
+    msg = req.body.message;
+    setData(nme, msg);
+
+    /*messageArray.push({"name": nme, "message": msg});
+    console.log(messageArray);*/
+
     res.sendStatus(200);
 })
 
 app.listen(port);
-console.log("server is listenong on port " + port);
+console.log("server is listening on port " + port);
